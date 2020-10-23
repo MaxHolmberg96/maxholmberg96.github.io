d90d25c2fae124d85a2f0fcf4cefda5fcbec4f1e
Max <max.holmberg96@gmail.com>
2020-10-23 13:54:01 +0200

    updated with real names for research categories

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+import json
+blacklist = ['cmp-lg', 'chao-dyn', 'supr-con', 'adap-org', 'q-alg', 'bayes-an', 'comp-gas', 'solv-int', 'acc-phys', 'patt-sol', 'plasm-ph', 'mtrl-th', 'dg-ga', 'chem-ph', 'ao-sci', 'funct-an', ]
+obj = json.load(open("catMap.json", "r"))
 options = open("option_select_picker.html", "w")
 data = open("allData.txt", "w")
 f = open("C:/Users/Max/Downloads/data_non_accumulative.csv", "r")
@@ -7,8 +10,14 @@ for i, line in enumerate(lines):
     columns = line.split(",")
     for j, col in enumerate(columns):
         if j == 0:
-            options.write("<option data-tokens=" + col + ">" + col + "</option>")
-            options.write("\n")
+            if col == '0':
+                continue
+            elif col in obj.keys():
+                options.write("<option data-tokens=" + obj[col] + ">" + obj[col] + "</option>")
+                options.write("\n")
+            else:
+                options.write("<option data-tokens=" + col + ">" + col + "</option>")
+                options.write("\n")
         else:
             if j == 1:
                 data.write("[")
